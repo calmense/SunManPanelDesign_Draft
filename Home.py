e4fd0d5ca1273cff41336e13024059d5b65a1fc2
Cal Mense <104301991+calmense@users.noreply.github.com>
2024-07-19 08:44:33 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,111 @@
+# Schraubenbemessungsprogramm: Webapp mit Streamlit - Axial- und Schertragfähigkeit von Würth Vollgewindeschrauben
+# Bibliotheken
+from math import pi, sqrt, cos, sin
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+from PIL import Image
+import geopandas as gpd
+import folium
+from streamlit_folium import folium_static
+from utils import *
+
+# from würth_screws_functions import get_length, ec5_87_tragfähigkeit_vg, get_min_distances_axial, get_min_distances_shear
+
+# HTML Einstellungen
+st.set_page_config(page_title="SunMan x ARUP", layout="wide")
+st.markdown("""<style>
+[data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 500px;}
+[data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 500px;margin-left: -500px;}
+footer:after{
+    content:"Arup Deutschland GmbH | SunMan Energy | Cal Mense";
+    display:block;
+    position:relative;
+    color:grey;
+}
+</style>""",unsafe_allow_html=True)
+
+st.markdown('''
+<style>
+.katex-html {
+    text-align: left;
+}
+</style>''',
+unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <style>
+    .subsubheader {
+        font-size: 1.5em; /* Adjust the font size as needed */
+        font-weight: bold;
+        margin-top: 0em; /* Adjust the margin as needed */
+        color: rgb(230, 30, 40); /* Change the color as needed */
+    }
+    .text {
+        font-size: 1.0em; /* Adjust the font size as needed */
+        margin-top: 0em; /* Adjust the margin as needed */
+        color: "black"; /* Change the color as needed */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("""
+    <style>
+    .header-button {
+        display: inline-block;
+        padding: 5px 15px;
+        font-size: 1.5rem;  /* Use a similar size as st.header */
+        font-weight: 600;  /* Bold to match the header style */
+        color: black;
+        background-color: #F0F2F6;  /* Change this to match your theme */
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        text-decoration: none;
+    }
+    .header-button:hover {
+        background-color: #F0F2F6;  /* Lighten color on hover */
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+original_title = '<p style="font-family:Times; font-size: 60px;"> <span style="color:black;"></span>SunMan x <span style="color:rgb(230, 30, 40);">ARUP</span></p>'
+st.image("Sunman_logo.png", width = 300)
+# st.markdown(original_title, unsafe_allow_html=True)
+
+# header = '<p style="font-family:Arial; color:rgb(0,0,0); font-size: 25px; font-weight: bold; ">SunMan Solar Panels - Ultra-light, Glass-free Technology</p>'
+# st.markdown(header, unsafe_allow_html=True)
+st.subheader("SunMan Solar Panels - Ultra-light, Glass-free Technology")
+st.write('This web tool provides a structural framework for adhering solar panels directly onto roofs without the need for screws. \
+         The panels are made from a durable, glass-free organic polymer composite that excels in various climatic conditions and extreme temperatures. \
+         Please note that the tool does not assume responsibility for any errors, and users are advised to verify the results independently.')
+
+st.write("")
+st.write("")
+st.write("")
+
+col1, col2 = st.columns([40,70])
+with col1:
+    st.image("title_image.png", width=450)
+with col2:
+
+
+    st.markdown('<h3 class="subsubheader">SunMan eArc - Ultra-light, Glass-free Technology</h3>', unsafe_allow_html=True)
+    st.markdown("""
+        <style>
+        .text {
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+            color: black;
+            line-height: 1.5;
+        }
+        </style>
+        <h3 class="text">
+            This document provides structural guidance for adhering solar panels directly to roofs without the need for screw penetrations.<br>
+            The solar panels utilize an organic polymer composite, devoid of glass, known for its exceptional durability in withstanding diverse climatic conditions and extreme temperatures.
+        </h3>
+    """, unsafe_allow_html=True)
---
+++ utils.py
@@ -0,0 +1,43 @@
+from math import pi, sqrt, cos, sin, atan
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+from itertools import product
+from collections import defaultdict
+
+
+def draw_arrow(fig, xList, yList, direction, scaleX, scaleY):
+    fig.add_trace(go.Scatter(x = xList, y = yList,
+                marker= dict(size=8, symbol= "arrow", angleref="previous", color='black'), line=dict(width=1)))
+       
+    fig.add_trace(go.Scatter(x = list(reversed(xList)), y = list(reversed(yList)),
+                marker= dict(size=8, symbol= "arrow", angleref="previous", color='black')))
+    
+    if direction == "X":
+        xLine1 = [xList[0], xList[0]]
+        yLine1 = [yList[0] - 5*scaleY, yList[0] + 5*scaleY]
+
+        xLine2 = [xList[-1], xList[-1]]
+        yLine2 = [yList[-1] - 5*scaleY, yList[-1] + 5*scaleY]
+
+    elif direction == "Y":
+        xLine1 = [xList[0] - 5*scaleY, xList[0] + 5*scaleY]
+        yLine1 = [yList[0], yList[0]]
+
+        xLine2 = [xList[-1] - 5*scaleY, xList[-1] + 5*scaleY]
+        yLine2 = [yList[-1], yList[-1]]
+
+    fig.add_trace(go.Scatter(x=xLine1, y = yLine1, mode="lines", marker=dict(color='black')))
+    fig.add_trace(go.Scatter(x=xLine2, y = yLine2, mode="lines", marker=dict(color='black', angle = 45)))
+
+
+def add_text(fig, text, xPosition, yPosition, textSize):
+    fig.add_annotation(dict(font=dict(size=textSize, color = "black"),
+                                            x = xPosition,
+                                            y = yPosition,
+                                            showarrow=False,
+                                            text=text,
+                                            textangle=0,
+                                            xanchor='left',
+                                            xref="x",
+                                            yref="y"))
